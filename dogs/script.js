16ef8a491afb12c2a568009b1ebd76dc40378979
Vania2007 <momotis2007@gmail.com>
2024-04-19 21:27:07 +0300

    update-dogs

--- dogs/script.js
+++ dogs/script.js
@@ -3,7 +3,29 @@ class Dog {
   age;
   weight;
   image;
+  openInfo() {
+    let width = 450;
+    let height = 380;
+    let leftpos = (screen.width - width) / 2;
+    let toppos = (screen.height - height) / 2;
+    let w = window.open(
+      `Dog.html`,
+      `Dog`,
+      `height=${height}, width=${width}, left=${leftpos}, top=${toppos}`
+    );
+    w.document.write(`<h1>${this.name}</h1>`);
+    w.document.write(`<img src="./images/${this.name}.jpg" alt="" />`);
+    w.document.write(`<p>Вік: ${this.age} років</p>`);
+    w.document.write(`<p>Вага: ${this.weight} кг</p>`);
+    w.document
+      .write(`<pre><strong>Послання від твого майбутнього друга:</strong>
+  Дуже хочу в нову сім'ю, сподіваюсь це будете Ви!</pre>`);
+    w.document.write(
+      `<p><input type=button value="Close" onclick="window.close()"/></p>`
+    );
+  }
 }
+
 let belka = new Dog();
 belka.name = "Belka";
 belka.age = 10;
@@ -27,24 +49,15 @@ rex.name = "Rex";
 rex.age = 11;
 rex.weight = 25;
 rex.image = "./images/rex.jpg";
-let dogs = [belka, black, jack, rex];
 
-function openInfo(name, width, height) {
-  let leftpos = (screen.width - width) / 2;
-  let toppos = (screen.height - height) / 2;
-  window.open(
-    `${name}.html`,
-    `${name}`,
-    `width=${width},height=${height}, left=${leftpos}, top=${toppos}`
-  );
-}
+let dogs = [belka, black, jack, rex];
 
 document.write(
   "<table><tr><th>Ім'я</th><th>Вік</th><th>Вага</th><th>Зображення</th><th>Додаткова інформація</th></tr>"
 );
 for (let i = 0; i < dogs.length; i++) {
   document.write(
-    `<tr><td>${dogs[i].name}</td><td>${dogs[i].age}</td><td>${dogs[i].weight}</td><td><img src="${dogs[i].image}" width='100'></td><td><input type="button" value="Показати інформацію" onclick="openInfo('${dogs[i].name}', 600, 500)"></input></td></tr>`
+    `<tr><td>${dogs[i].name}</td><td>${dogs[i].age}</td><td>${dogs[i].weight}</td><td><img src="${dogs[i].image}" width='100'></td><td><input type="button" value="Показати інформацію" onclick="dogs[${i}].openInfo()"></input></td></tr>`
   );
 }
 document.write("</table>");
